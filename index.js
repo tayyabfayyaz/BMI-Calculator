b5f854e66f84212825c179e2525307db3a1f4e67
Your Name <fayyaztayyab684@gmail.com>
2024-03-18 04:00:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Tayyab Fayyaz 
+// It's a BMI calculator 
+let weightInKiloGram = 45;
+let heightInMeter = 2.5;
+let result = weightInKiloGram / (heightInMeter * heightInMeter);
+console.log(`Your BMI is: ${result}`);
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Tayyab Fayyaz 
+// It's a BMI calculator 
+
+let weightInKiloGram: number = 45;
+let heightInMeter: number = 2.5;
+
+let result: number = weightInKiloGram / (heightInMeter * heightInMeter);
+
+console.log(`Your BMI is: ${result}`);
